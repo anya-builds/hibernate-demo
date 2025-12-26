0bad0bd75f74828d868c323ac51d8fea0e3d19a2
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-12-26 17:55:22 +0530

    hibernate in spring

---
+++ src/main/java/HibernateUtil.java
@@ -0,0 +1,17 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+//import javax.security.auth.login.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    static  {
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.cgf.xml")
+                    .addAnnotatedClass()
+                    .buildSessionFactory();
+        }
+    }
+}
